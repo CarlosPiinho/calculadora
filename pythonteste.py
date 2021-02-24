7b439d33c834d21980e9f8889e614fa8971957d4
CarlosPiinho <62005227+CarlosPiinho@users.noreply.github.com>
2021-02-24 19:00:41 -0300

    Add files via upload

---
+++ pythonteste.py
@@ -0,0 +1,22 @@
+tabuada = int(input('Digite o número, que realizaremos o cálculo para você: '))
+n1 = tabuada * 1
+n2 = tabuada * 2
+n3 = tabuada * 3
+n4 = tabuada * 4
+n5 = tabuada * 5
+n6 = tabuada * 6
+n7 = tabuada * 7
+n8 = tabuada * 8
+n9 = tabuada * 9
+
+print('--------------')
+print('{} x 1 = {}'.format(tabuada, n1))
+print('{} x 2 = {}'.format(tabuada, n2))
+print('{} x 3 = {}'.format(tabuada, n3))
+print('{} x 4 = {}'.format(tabuada, n4))
+print('{} x 5 = {}'.format(tabuada, n5))
+print('{} x 6 = {}'.format(tabuada, n6))
+print('{} x 7 = {}'.format(tabuada, n7))
+print('{} x 8 = {}'.format(tabuada, n8))
+print('{} x 9 = {}'.format(tabuada, n9))
+print('--------------')
